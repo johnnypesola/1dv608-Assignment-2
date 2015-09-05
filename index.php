435c5f2a5d71b7b9851f7b738da1b90ba3d56ded
Johnny Pesola <johnny@pesola.eu>
2015-09-06 01:10:38 +0200

    Code refactoring

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
-//INCLUDE THE FILES NEEDED...
+// Include files
 require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
 require_once('view/LayoutView.php');
 
-//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
+// Show errors from server. Turn off on public servers.
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-//CREATE OBJECTS OF THE VIEWS
-$v = new LoginView();
-$dtv = new DateTimeView();
-$lv = new LayoutView();
+// Create objects of views
+$loginViewObj = new \view\LoginView();
+$dateTimeViewObj = new \view\DateTimeView();
+$layoutViewObj = new \view\LayoutView('Login Example', 'Assignment 2');
 
 
-$lv->render(false, $v, $dtv);
+$layoutViewObj->render(false, $loginViewObj, $dateTimeViewObj);
 
--- view/LayoutView.php
+++ view/LayoutView.php
@@ -1,35 +1,64 @@
 <?php
 
 
+namespace view;
+
 class LayoutView {
-  
-  public function render($isLoggedIn, LoginView $v, DateTimeView $dtv) {
-    echo '<!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <title>Login Example</title>
-        </head>
-        <body>
-          <h1>Assignment 2</h1>
-          ' . $this->renderIsLoggedIn($isLoggedIn) . '
-          
-          <div class="container">
-              ' . $v->response() . '
-              
-              ' . $dtv->show() . '
-          </div>
-         </body>
-      </html>
-    ';
-  }
-  
-  private function renderIsLoggedIn($isLoggedIn) {
-    if ($isLoggedIn) {
-      return '<h2>Logged in</h2>';
+
+    private $pageTitle = '';
+    private $pageCharset = '';
+    private $pageHeader = '';
+
+    public function __construct($pageTitle, $pageHeader, $pageCharset = 'utf-8') {
+
+        // Set values on object creation
+        $this->pageTitle = $pageTitle;
+        $this->pageHeader = $pageHeader;
+        $this->pageCharset = $pageCharset;
     }
-    else {
-      return '<h2>Not logged in</h2>';
+
+
+    public function render($isLoggedIn, LoginView $loginViewObj, DateTimeView $dateTimeViewObj) {
+
+        // Render page header
+        $this->renderHeader();
+
+        // Render text about if the user is logged in or not
+        $this->renderIsLoggedIn($isLoggedIn);
+
+        echo '
+              <div class="container">
+                  ' . $loginViewObj->response() . '
+
+                  ' . $dateTimeViewObj->show() . '
+              </div>
+        ';
+
+        // Render page footer
+        $this->renderFooter();
     }
-  }
+
+    private function renderHeader() {
+        echo '
+            <!DOCTYPE html>
+                  <html>
+                    <head>
+                      <meta charset="' . $this->pageCharset . '">
+                      <title>' . $this->pageTitle . '</title>
+                    </head>
+                    <body>
+                      <h1>' . $this->pageHeader . '</h1>
+        ';
+    }
+
+    private function renderFooter() {
+        echo '
+                     </body>
+                  </html>
+        ';
+    }
+  
+    private function renderIsLoggedIn($isLoggedIn) {
+        echo ($isLoggedIn ? '<h2>Logged in</h2>' : '<h2>Not logged in</h2>');
+    }
 }
